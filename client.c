c525580a82fa0b9a649f5d902f77c112b713cfa2
MataBM <112943944+MataBM@users.noreply.github.com>
2022-11-03 16:41:08 +0000

    almost done

--- client.c
+++ client.c
@@ -7,24 +7,29 @@ void	the_butcher(int pid, char *msg)
 {
 	int	byte;
 	int	i;
-	char chr;
 
 	i = -1;
 	while (msg[++i])
 	{
-		byte = 8;
-		chr = msg[i];
-		while (byte)
+		byte = 7;
+		while (byte >= 0)
 		{
-			if (chr % 2)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			chr /= 2;
-			byte--;
+			if (msg[i] >> byte & 1)
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					{
+						write(2, "ERRO\nWrong PID", 15);
+						exit (0);
+					}
+			}
+			else if (kill(pid, SIGUSR2) == -1)
+			{
+				write(2, "ERRO\nWrong PID", 15);
+				exit (0);
+			}
 			usleep(1);
+			byte--;
 		}
-		kill(pid, SIGCONT);
 	}
 }
 
@@ -35,5 +40,11 @@ int main(int arg_c, char **arg_v)
 		write(2, "ERROR\nNOT ENOUGH ARGUMENTS\n", 27);
 		return (1);
 	}
+	if (ft_isalnum(ft_atoi(arg_v[1])))
+	{
+		write(2, "ERROR\nPID ISN'T NUM\n", 27);
+		return (1);
+	}
 	the_butcher(ft_atoi(arg_v[1]), arg_v[2]);
+	return (0);
 }
